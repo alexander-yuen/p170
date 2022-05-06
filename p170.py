13ca2091ea043792f25b7030645df9427f64e06a
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2022-05-06 16:16:45 -0400

    Add files via upload

---
+++ p170.py
@@ -0,0 +1,34 @@
+class Grade_3():
+    def percentage3(self):
+        print("\n","Grade Percentages","\n")
+        print("45.0")
+        
+    def year_1976(self):
+        print("\n","45.0","\n")
+        
+class Grade_4():
+    def percentage4(self):
+        print("\n","60.0","\n")
+    
+    def year_1977(self):
+        print("\n","60.0","\n")
+
+class Grade_5():
+    def percentage5(self):
+        print("\n","","\n")
+    
+    def year_1978(self):
+        print("\n","62.5","\n")
+        
+
+grade3 = Grade_3()
+
+grade3.year_1976()
+
+grade4 = Grade_4()
+
+grade4.year_1977()
+
+grade5 = Grade_5()
+
+grade5.year_1978()
